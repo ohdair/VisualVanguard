b03de68bc8cb478ad6333922d8e153708e28d4ab
ohdair <ohdiar@gmail.com>
2024-07-03 22:40:07 +0900

    init: project 생성

---
+++ VisualVanguard/VisualVanguard/VisualVanguardApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisualVanguardApp.swift
+//  VisualVanguard
+//
+//  Created by 박재우 on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct VisualVanguardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
